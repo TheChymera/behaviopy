1a2e711f6917ccb2d3d6c6e809ab883e7d145000
Horea Christian <horea.christ@yandex.com>
2016-08-24 04:16:37 +0200

    working subsection center and crop

--- behaviopy/event_tracking.py
+++ behaviopy/event_tracking.py
@@ -1,6 +1,13 @@
 from psychopy import core, visual, data, event
 from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED, STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
 
+
+x0 = 0.
+x1 = 0.25
+
+y0 = 0.
+y1 = 1.
+
 # Store info about the experiment session
 expName = u'untitled'  # from the Builder filename that created this script
 expInfo = {'participant':'', 'session':'001'}
@@ -11,28 +18,41 @@ endExpNow = False  # flag for 'escape' or other condition => quit the exp
 
 win = visual.Window(
 	size=(2048, 1152), fullscr=True, screen=0,
-	allowGUI=False, allowStencil=False,
+	allowGUI=False, allowStencil=True,
 	monitor='testMonitor', color=[0,0,0], colorSpace='rgb',
 	blendMode='avg', useFBO=True,
+	units="norm",
 	)
-# store frame rate of monitor if we can measure it
-expInfo['frameRate'] = win.getActualFrameRate()
-if expInfo['frameRate'] != None:
-	frameDur = 1.0 / round(expInfo['frameRate'])
-else:
-	frameDur = 1.0 / 60.0  # could not measure, so guess
+
+aperture_x = 0.2
+aperture_y = 1
 
 # Initialize components for Routine "trial"
 trialClock = core.Clock()
-movie = visual.MovieStim(
-	win=win, name='movie',
+mov = visual.MovieStim(
+	win=win, name='mov',
 	filename=u'/home/chymera/data/cameras/nd750/a/nd750_a0037.mkv',
-	ori=0, pos=(0, 0), opacity=1,
+	ori=0, opacity=1,
 	depth=0.0,
 	volume=0,
 	units="norm",
-	size=1.7
+	size=2,
 	)
+
+mov_xsize, mov_ysize = mov.size
+
+movsection_xpos = (x0+x1)/2.
+movsection_ypos = (y0+y1)/2.
+
+mov_xpos = mov_xsize*(1./2.-movsection_xpos)
+mov_ypos = mov_ysize*(1./2.-movsection_ypos)
+
+mov.pos = (mov_xpos,mov_ypos)
+
+aperture_x = movsection_xpos*2*mov_xsize
+aperture_y = movsection_ypos*2*mov_ysize
+
+aperture = visual.Aperture(win, size=(aperture_x,aperture_y), pos=(0, 0), ori=0, nVert=120, shape='square', inverted=False, name=None, autoLog=None)
 
 # Create some handy timers
 globalClock = core.Clock()  # to track the time since experiment started
@@ -46,7 +66,7 @@ continueRoutine = True
 routineTimer.add(30.000000)
 # update component parameters for each repeat
 # keep track of which components have finished
-trialComponents = [movie]
+trialComponents = [mov]
 for thisComponent in trialComponents:
 	if hasattr(thisComponent, 'status'):
 		thisComponent.status = NOT_STARTED
@@ -58,15 +78,15 @@ while continueRoutine and routineTimer.getTime() > 0:
 	frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
 	# update/draw components on each frame
 
-	# *movie* updates
-	if t >= 0.0 and movie.status == NOT_STARTED:
+	# *mov* updates
+	if t >= 0.0 and mov.status == NOT_STARTED:
 		# keep track of start time/frame for later
-		movie.tStart = t
-		movie.frameNStart = frameN  # exact frame index
-		movie.setAutoDraw(True)
+		mov.tStart = t
+		mov.frameNStart = frameN  # exact frame index
+		mov.setAutoDraw(True)
 	frameRemains = 0.0 + 30.0- win.monitorFramePeriod * 0.75  # most of one frame period left
-	if movie.status == STARTED and t >= frameRemains:
-		movie.setAutoDraw(False)
+	if mov.status == STARTED and t >= frameRemains:
+		mov.setAutoDraw(False)
 
 	# check if all components have finished
 	if not continueRoutine:  # a component has requested a forced-end of Routine
