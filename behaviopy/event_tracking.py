9b0490b7100a4495d22e371d64d7c25bcee5d9ac
Horea Christian <horea.christ@yandex.com>
2016-08-26 17:03:10 +0200

    first working draft of evaluate_db

--- behaviopy/event_tracking.py
+++ behaviopy/event_tracking.py
@@ -3,10 +3,62 @@ import os
 from psychopy import core, visual, data, event
 from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED, STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
 
-def evaluate_db():
-	return
+def evaluate_db(db_path, test_type, animal_ids=[], animals_id_column="id_eth", dates=[], output_dir="~/data/behaviour", author="", volume=0):
+	"""Wrapper for evaluate() passing data from a LabbookDB model database.
 
-def evaluate(recording_path, trial_duration, events={},bracket="", volume=1.):
+	"""
+	import sys
+	sys.path.append(os.path.expanduser('~/src/LabbookDB/db/'))
+	from query import get_df
+
+	if not author:
+		print("It is advisable to add your name's three-letter abbreviation via the \"author\" argument. This helps identify your work and protects it from being overwritten.")
+
+	db_path = os.path.expanduser(db_path)
+	output_dir = os.path.expanduser(output_dir)
+
+	if test_type == "forced_swim_test":
+		measurements_table = "ForcedSwimTestMeasurement"
+		output_dir = os.path.join(output_dir,"forced_swim_test")
+		trial_duration = 360 #in seconds
+		events = {"s":"swimming","f":"floating"}
+	else:
+		raise ValueError("The function does not support test_type=\'"+test_type+"\'.")
+
+
+	col_entries=[
+		("Animal","id"),
+		(measurements_table,),
+		]
+	join_entries=[
+		("Animal.measurements",),
+		(measurements_table,),
+		]
+
+	filters = []
+	if animal_ids:
+		filter_entry = ["Animal",animals_id_column,]
+		for animal_id in animal_ids:
+			filter_entry.extend([animal_id])
+		filters.append(filter_entry)
+	if dates:
+		filter_entry = ["measurements_table","date"]
+		for date in dates:
+			filter_entry.extend([date])
+		filters.append(filter_entry)
+
+	reference_df = get_df(db_path,col_entries=col_entries, join_entries=join_entries, filters=[])
+	for _, measurement_df in reference_df.iterrows():
+		recording_path = measurement_df.loc[measurements_table+"_recording"]
+		if measurements_table+"_recording_bracket" in reference_df.columns:
+			bracket = measurement_df.loc[measurements_table+"_recording_bracket"]
+		else:
+			bracket = ""
+		outfile_name = os.path.splitext(os.path.basename(recording_path))[0]+"_"+bracket+"_"+author
+		output_path = os.path.join(output_dir,outfile_name)
+		evaluate(recording_path,trial_duration,events=events,bracket=bracket, volume=volume, output_path=output_path)
+
+def evaluate(recording_path, trial_duration, skiptime=0, events={},bracket="", volume=1.,output_path="~/evaluation.csv"):
 	"""Evaluate a behavioural recording.
 
 	Parameters
@@ -19,30 +71,30 @@ def evaluate(recording_path, trial_duration, events={},bracket="", volume=1.):
 	"""
 
 	recording_path = os.path.expanduser(recording_path)
+	output_path = os.path.expanduser(output_path)
+
+	if os.path.exists(output_path):
+		print("There is already a n evaluation file at "+output_path+". Please specify a different output path.")
+		return
 
 	keylist = ['left','right','up','down','escape','return']
 	experiment_keylist = events.keys()
 	keylist.extend(experiment_keylist)
 
-	#set up evaluation data handling
-	# trials = data.TrialHandler([{"a":"a"}], 1)
-	# trials.data.addDataType('event')  # this will help store things with the stimuli
-	# trials.data.addDataType('time')  # add as many types as you like
-
 	#process brackets string
 	x_y_brackets = bracket.split(",")
 	if len(x_y_brackets) == 1:
-		x0, x1 = [float(i) for i in x_y_brackets[0].split("-")]
+		x0, x1 = [float(i)/100 for i in x_y_brackets[0].split("-")]
 		y0 = 0.
 		y1 = 1.
 	elif len(x_y_brackets) == 2:
 		try:
-			x0, x1 = [float(i) for i in x_y_brackets[0].split("-")]
+			x0, x1 = [float(i)/100 for i in x_y_brackets[0].split("-")]
 		except ValueError:
 			x0 = 0.
 			x1 = 1.
 		try:
-			y0, y1 = [float(i) for i in x_y_brackets[1].split("-")]
+			y0, y1 = [float(i)/100 for i in x_y_brackets[1].split("-")]
 		except ValueError:
 			y0 = 0.
 			y1 = 1.
@@ -57,7 +109,7 @@ def evaluate(recording_path, trial_duration, events={},bracket="", volume=1.):
 
 	# Initialize components for Routine "trial"
 	trialClock = core.Clock()
-	mov = visual.MovieStim(
+	mov = visual.MovieStim2(
 		win=win, name='mov',
 		filename=recording_path,
 		depth=0.0,
@@ -92,7 +144,8 @@ def evaluate(recording_path, trial_duration, events={},bracket="", volume=1.):
 	mov.status = NOT_STARTED
 
 	pre_evaluation = True
-	responses = []
+	outputWriter1 = csv.writer(open(output_path,'w'), lineterminator ='\n')
+	outputWriter1.writerow(["start","behaviour"])
 	# -------Start Routine "trial"-------
 	while pre_evaluation or routineTimer.getTime() > 0:
 		# get current time
@@ -106,12 +159,8 @@ def evaluate(recording_path, trial_duration, events={},bracket="", volume=1.):
 		elif resp_key:
 			if resp_key[0] in experiment_keylist:
 				tempArray = [t, events[resp_key[0]]]
-				responses.append(tempArray)
-				# trials.addData('event', resp_key[0])  # add the data to our set
-				# trials.addData('time', t)
-				# trials.data.add('event', resp_key[0])  # add the data to our set
-				# trials.data.add('time', t)
-
+				outputWriter1.writerow(tempArray)
+				tempArray =[] #make sure no info persists
 
 		#start movie and keep track of start time
 		if t >= 0.0 and mov.status == NOT_STARTED:
@@ -127,25 +176,15 @@ def evaluate(recording_path, trial_duration, events={},bracket="", volume=1.):
 
 		win.flip()
 
-	print responses
-	outputWriter1 = csv.writer(open('/home/chymera/src/behaviopy/behaviopy/fileName.csv','w'), lineterminator ='\n')
+
 	for i in range(0, len(responses)):
-		outputWriter1.writerow(responses[i])
-	# trials.printAsText(stimOut=['a'],dataOut=['event', 't'])
-	# a = trials.saveAsWideText("testDataWide.txt")
-	# print a
+		(responses[i])
 	win.close()
 
 if __name__ == '__main__':
-	recording_path =u"/home/chymera/data/cameras/nd750/a/nd750_a0037.mkv"
-	recording_path =u"/home/chymera/data/cameras/nd750/a/nd750_a0038.mkv"
+	# recording_path =u"/home/chymera/data/cameras/nd750/a/nd750_a0037.mkv"
+	# recording_path =u"/home/chymera/data/cameras/nd750/a/nd750_a0038.mkv"
 	recording_path =u"/home/chymera/data/cameras/nd750/a/nd750_a0039.mkv"
-	bracket = "0-.24,"
-	bracket = ".24-.42,"
-	bracket = ".42-.59,"
-	bracket = ".59-.77,"
-	bracket = ".77-1,"
-	bracket = ".4-.58,"
-	bracket = ".2-.4,"
-	bracket = ".58-.75,"
-	evaluate(recording_path,5,events={"s":"swimming","f":"floating"}, bracket=bracket, volume=0)
+	bracket = "58-75,"
+	evaluate(recording_path,5,events={"s":"swimming","f":"floating"}, bracket=bracket, volume=0.0001)
+	# evaluate_db("~/syncdata/meta.db","forced_swim_test",animal_ids=[275511],author="chr",volume=0.0001)
