1f23bb7f0633f0979fbdc8a711fc076ef345298c
Horea Christian <horea.christ@yandex.com>
2016-09-01 13:17:12 +0200

    migrated calculations to labbookdb.report.processing

--- behaviopy/utils.py
+++ behaviopy/utils.py
@@ -34,10 +34,3 @@ def add_significance(df, datacolumn, compare, over):
 		x2 = ix+0.2
 		plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=1, c=col)
 		plt.text((x1+x2)*0.5, y+h, "p = {:.2g}".format(p_value), ha='center', va='bottom', color=col)
-
-def rounded_days(datetime_obj):
-	days = datetime_obj.days
-	# rounding number of days up, as timedelta.days returns the floor only:
-	if datetime_obj.seconds / 43199.5 >= 1:
-		days += 1
-	return days
