f45aa1198224eea70bcfa45b60a1f17464ce3423
Horea Christian <horea.christ@yandex.com>
2017-03-25 01:43:19 +0100

    added examples

--- behaviopy/examples.py
+++ behaviopy/examples.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 from os import path
 
+import plotting
 import regression
 from abbreviations import *
 from utils import get_data_dir
@@ -53,7 +55,59 @@ def pet_behaviour(
 		behav_normalize=False,
 		)
 
+def forced_swim_ttest():
+	data_dir = get_data_dir("generic", DATA_ROOT_VARIANTS)
+	df_path = path.join(data_dir,"forcedswim_bins.csv")
+	df = pd.read_csv(df_path)
+
+	plotting.expandable_ttest(df,
+		compare="Treatment",
+		comparisons={"Interval [minutes]":[]},
+		datacolumn_label="Immobility Ratio",
+		rename_treatments={"cFluDW":"Fluoxetine","cFluDW_":"Control"},
+		)
+
+def forced_swim_ts(plot_style):
+	data_dir = get_data_dir("generic", DATA_ROOT_VARIANTS)
+	df_path = path.join(data_dir,"forcedswim_ts.csv")
+	df = pd.read_csv(df_path)
+
+	plotting.forced_swim_timecourse(df,
+		time_label="Interval [1 min]",
+		plotstyle=plot_style,
+		datacolumn_label="Immobility Ratio",
+		)
+
+def sucrose_preference_treatment():
+	data_dir = get_data_dir("generic", DATA_ROOT_VARIANTS)
+	df_path = path.join(data_dir,"sucrosepreference_treatment.csv")
+	df = pd.read_csv(df_path)
+
+	plotting.expandable_ttest(df,
+		compare="Treatment",
+		comparisons={"Period [days]":[]},
+		datacolumn_label="Sucrose Preference Ratio",
+		rename_treatments={"cFluDW":"Fluoxetine","cFluDW_":"Control"},
+		)
+
+def sucrose_preference_side():
+	data_dir = get_data_dir("generic", DATA_ROOT_VARIANTS)
+	df_path = path.join(data_dir,"sucrosepreference_side.csv")
+	df = pd.read_csv(df_path)
+
+	plotting.expandable_ttest(df,
+		compare="Sucrose Bottle Position",
+		comparisons={"Cage ID":[]},
+		datacolumn_label="Sucrose Preference Ratio",
+		rename_treatments={"cFluDW":"Fluoxetine","cFluDW_":"Control"},
+		)
+
 if __name__ == '__main__':
-	pet_behaviour()
+	# pet_behaviour()
+	# forced_swim_ttest()
+	forced_swim_ts("tsplot")
+	# forced_swim_ts("pointplot")
+	# sucrose_preference_side()
+	# sucrose_preference_treatment()
 
 	plt.show()
