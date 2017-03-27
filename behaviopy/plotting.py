5162034003b673ef5a02be4a2b3176145fc0aeb7
Horea Christian <horea.christ@yandex.com>
2017-03-27 14:57:23 +0200

    integrated timetableplot into behaviopy

--- behaviopy/plotting.py
+++ behaviopy/plotting.py
@@ -1,22 +1,182 @@
 __author__="Horea Christian"
 import sys
-import pandas as pd
-import matplotlib.pyplot as plt
-import datetime
 import time
+import datetime as dt
+import pandas as pd
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
 from os import path
 from sqlalchemy import create_engine, or_, inspection
+from matplotlib.colors import ListedColormap
 from sqlalchemy.orm import sessionmaker, aliased, with_polymorphic, joinedload_all
 
 try:
 	from .utils import *
+	from .plot_utils import *
 except ValueError:
 	from utils import *
+	from plot_utils import *
+
 
 import seaborn as sns
 sns.set_style("white", {'legend.frameon': True})
 
 qualitative_colorset = ["#000000", "#E69F00", "#56B4E9", "#009E73","#F0E442", "#0072B2", "#D55E00", "#CC79A7"]
+
+def timetable(reference_df, x_key, shade, saturate,
+	colorlist=["0.9","#fff3a3","#a3e0ff","#ffa3ed","#ffa3a3"],
+	padding=3,
+	real_dates=True,
+	window_start="",
+	window_end="",
+	save_plot=""
+	):
+	"""Plot a timetable
+
+	Parameters
+	----------
+
+	reference_df : Pandas DataFrame
+	Dataframe containing the data to plot. It needs to contain columns with datetime values.
+
+	x_key : string
+	Column from `reference_df` for the values in which to create rows in the timetable.
+
+	shade: {list of str, list of dict}
+	Strings specify the columns for which to shade the datetimes. In dictionaries, the key gives a column to filter by; and if the first item in the value list matches the column value, the datetime in the second item in the value list will specify which datetimes to shade; if the value list contains three items, the datetimes in between that in the second item column and the third item column will be shaded.
+
+	saturate: {list of str, list of dict}
+	Strings specify the columns for which to saturate the datetimes. In dictionaries, the key gives a column to filter by; and if the first item in the value list matches the column value, the datetime in the second item in the value list will specify which datetimes to saturate; if the value list contains three items, the datetimes in between that in the second item column and the third item column will be shaded.
+
+	colorlist : list
+	A list containing matplotlib-compatible colors to be used for shading.
+
+	padding : int
+	Number of days to bad the timetable window with (before and after the first and last scan respectively).
+
+	real_dates : boolean
+	Set to False to display dates relative to the first measurement.
+
+	window_start : string
+	A datetime-formatted string (e.g. "2016,12,18") to apply as the timetable start date (overrides autodetected start).
+
+	window_end : string
+	A datetime-formatted string (e.g. "2016,12,18") to apply as the timetable end date (overrides autodetected end).
+	"""
+
+	#truncate dates
+	for col in reference_df.columns:
+		if "date" in col:
+			#the following catches None entries:
+			try:
+				reference_df[col] = reference_df[col].apply(lambda x: x.date())
+			except AttributeError:
+				pass
+
+	#GET FIRST AND LAST DATE
+	dates = get_dates(reference_df, [shade, saturate])
+	if not window_start:
+		window_start = min(dates) - dt.timedelta(days=padding)
+	else:
+		window_start = dt.strptime(window_start, "%Y,%m,%d").date()
+	if not window_end:
+		window_end = max(dates) + dt.timedelta(days=padding)
+	else:
+		window_end = dt.strptime(window_end, "%Y,%m,%d").date()
+
+	#create generic plotting dataframe
+	x_vals = list(set(reference_df[x_key]))
+	datetime_index = [i for i in perdelta(window_start,window_end,dt.timedelta(days=1))]
+
+	df = pd.DataFrame(index=datetime_index, columns=x_vals)
+	df = df.fillna(0)
+
+	#set plotting params
+	cMap = add_color(cm.viridis, 0.9)
+	fig_shape = (df.shape[0],df.shape[1]/1.5) #1.5 seems like a good scaling value to make cells not-too-tall and not-too-short
+	fig, ax = plt.subplots(figsize=fig_shape , facecolor='#eeeeee', tight_layout=True)
+
+	#populate frames
+	df_ = df.copy(deep=True)
+	for c_step, entry in enumerate(shade):
+		c_step += 1
+		for x_val in x_vals:
+			if isinstance(entry, dict):
+				for key in entry:
+					start=False #unless the code below is succesful, no attempt is made to add an entry for the x_val
+					filtered_df = reference_df[(reference_df[key] == entry[key][0])&(reference_df[x_key] == x_val)]
+					try:
+						start = list(set(filtered_df[entry[key][1]]))[0]
+					except IndexError:
+						pass
+					if len(entry[key]) == 3:
+						end = list(set(filtered_df[entry[key][2]]))[0]
+						active_dates = [i for i in perdelta(start,end+dt.timedelta(days=1),dt.timedelta(days=1))]
+						for active_date in active_dates:
+							df_.set_value(active_date, x_val, df_.get_value(active_date, x_val)+c_step)
+					elif start:
+						df_.set_value(start, x_val, df_.get_value(start, x_val)+c_step)
+			elif isinstance(entry, str):
+				filtered_df = reference_df[reference_df[x_key] == x_val]
+				active_dates = list(set(filtered_df[entry]))
+				for active_date in active_dates:
+					#escaping dates which are outside the date range (e.g. when specifying tighter window_end and window_start contraints)
+					try:
+						df_.set_value(active_date, x_val, df_.get_value(active_date, x_val)+c_step)
+					except KeyError:
+						pass
+	if not real_dates:
+		df_ = df_.set_index(np.arange(len(df_))-padding)
+	im = ax.pcolorfast(df_.T, cmap=add_color(cm.gray_r, 0.8), alpha=.5)
+	plt.hold(True)
+
+	#populate frames
+	df_ = df.copy(deep=True)
+	for c_step, entry in enumerate(saturate):
+		c_step += 1
+		for x_val in x_vals:
+			if isinstance(entry, dict):
+				for key in entry:
+					filtered_df = reference_df[(reference_df[key] == entry[key][0])&(reference_df[x_key] == x_val)]
+					try:
+						start = list(set(filtered_df[entry[key][1]]))[0]
+						try:
+							start = start.date()
+						except AttributeError:
+							pass
+					except IndexError:
+						pass
+					if len(entry[key]) == 3:
+						try:
+							end = list(set(filtered_df[entry[key][2]]))[0]
+							active_dates = [i for i in perdelta(start,end+dt.timedelta(days=1),dt.timedelta(days=1))]
+							for active_date in active_dates:
+								df_.set_value(active_date, x_val, df_.get_value(active_date, x_val)+c_step)
+						except IndexError:
+							pass
+					elif start:
+						df_.set_value(start, x_val, df_.get_value(start, x_val)+c_step)
+					# we need this to make sure start does not remain set for the next iteration:
+					start=False
+			elif isinstance(entry, str):
+				filtered_df = reference_df[reference_df[x_key] == x_val]
+				active_dates = list(set(filtered_df[entry]))
+				df_.set_value(active_dates, x_val, 1)
+	if not real_dates:
+		df_ = df_.set_index(np.arange(len(df_))-padding)
+	im = ax.pcolorfast(df_.T, cmap=ListedColormap(colorlist), vmin=0, vmax=len(colorlist)-1, alpha=.5)
+	plt.hold(True)
+
+	if real_dates:
+		ax = ttp_style(ax, df_)
+	else:
+		ax = ttp_style(ax, df_, padding)
+		plt.xlabel("Days")
+	plt.ylabel(" ".join(x_key.split("_")).replace("id","ID"))
+
+	if save_plot:
+		plt.savefig(path.abspath(path.expanduser(save_plot)), bbox_inches='tight')
 
 def control_first_reordering(df, hue_column):
 	"""Reorder the dataframe so that rows with control data show up first (this makes Seaborn assign them the first colors in the colorset).
--- behaviopy/utils.py
+++ behaviopy/utils.py
@@ -1,10 +1,6 @@
 __author__="Horea Christian"
-import collections
 import pandas as pd
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 from os import path
-from scipy import stats
 
 def get_data_dir(subdirectory, data_root_variants):
 	"""Get absolute data directory contingent on what variants are available on the system.
@@ -30,42 +26,30 @@ def get_data_dir(subdirectory, data_root_variants):
 
 	return my_data_dir
 
-def add_significance(df, datacolumn, compare, over):
-	"""Print significance level on 2-item comparison plots (also works with multiple 2-item comparisons)
+def perdelta(start, end, delta):
+	curr = start
+	while curr < end:
+		yield curr
+		curr += delta
+	return
 
-	Parameters
-	----------
-
-	df: pandas_dataframe
-		A long-format pandas dataframe containing the data used for the plot.
-	datacolumn: string
-		The column on which the plotted dimension is located.
-	compare: string
-		The column over which to perform the t-test comparison (the data frame MUST have two and only two values on this column).
-	over: string
-		The column over which the plot is repeated. For each value in this column a new t-test is performed. NO correction is performed for multiple comparisons!
-	"""
-
-
-	y, h, col = df[datacolumn].max() + 0.1, 0.1, '0.4'
-	comparisons = list(collections.OrderedDict.fromkeys(df[over]))
-	compare_categories = list(set(df[compare]))
-	line_width = mpl.rcParams['lines.linewidth']
-	label_size = mpl.rcParams['axes.labelsize']
-	for ix, comparison in enumerate(comparisons):
-		compare_vals=[]
-		for compare_category in compare_categories:
-			compare_category_vals = df.loc[(df[compare] == compare_category) & (df[over] == comparison)][datacolumn]
-			compare_vals.append(compare_category_vals)
-		compare_vals_lengths = [len(i) for i in compare_vals]
-		if 1 in compare_vals_lengths:
-			compare_vals = [i for (j,i) in sorted(zip(compare_vals_lengths,compare_vals), reverse=True)]
-			_, p_values = stats.ttest_1samp(*compare_vals)
-			p_value = p_values[0]
-		else:
-			_, p_value = stats.ttest_ind(*compare_vals)
-
-		x1 = ix-0.2
-		x2 = ix+0.2
-		plt.plot([x1, x1, x2, x2], [y, y+h, y+h, y], lw=line_width/3, c=col)
-		plt.text((x1+x2)*0.5, y+h, "p = {:.2g}".format(p_value), ha='center', va='bottom', color=col, fontsize=label_size)
+def get_dates(df, parameters):
+	dates=[]
+	for parameter in parameters:
+		for entry in parameter:
+			if isinstance(entry, str):
+				notnull_df = df[pd.notnull(df[entry])]
+				dates.extend(list(set(notnull_df[entry])))
+			if isinstance(entry, dict):
+				for key in entry:
+					filtered_df = df[df[key] == entry[key][0]]
+					for col in entry[key][1:]:
+						dates.extend(list(set(filtered_df[col])))
+	dates = list(set(dates))
+	checked_dates=[]
+	for dt in dates:
+		try:
+			checked_dates.append(dt.date())
+		except AttributeError:
+			checked_dates.append(dt)
+	return checked_dates
