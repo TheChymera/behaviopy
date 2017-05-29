fc04a4a2547704a6f61101c2ae9446b39ccb21ca
Horea Christian <horea.christ@yandex.com>
2017-05-29 05:27:25 +0200

    ready for packaging

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+if __name__ == '__main__':
+	setup(
+		name="BehavioPy",
+		version="",
+		description = "Python evaluation (e.g. event tracking) and plotting functions for behavioural data.",
+		author = "Horea Christian",
+		author_email = "horea.christ@gmail.com",
+		url = "https://github.com/TheChymera/behaviopy",
+		keywords = ["plotting", "science", "data analysis", "analysis", "reports", "statistics", "behaviour"],
+		classifiers = [],
+		install_requires = [],
+		provides = ["behaviopy"],
+		packages = ["behaviopy"],
+		)
