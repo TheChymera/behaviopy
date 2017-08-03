a83324d5d77db44ed3723873510e0d90fdbdf6aa
Horea Christian <horea.christ@yandex.com>
2017-08-03 02:53:23 +0200

    deleted stable example cobered under examples module

--- behaviopy/development.py
+++ behaviopy/development.py
@@ -12,49 +12,6 @@ DATA_ROOT_VARIANTS = [
 	path.join(THISSCRIPTSPATH,"..","example_data"),
 	]
 
-def pet_behaviour(
-	x_cols=["amygdala", "bfs", "brain stem", "striatum", "cerebellum", "central gyrus", "inferior colliculus", "superior colliculus", "cortex", "hippocampus", "hypothalamus", "midbrain", "olfactory", "thalamus",],
-	y_cols=["assisted rearing", "unassisted rearing", "wall walking", "center walking", "object interaction", "body grooming", "head grooming", "risk assessment", "immobility"],
-	):
-
-	import regression
-
-	data_dir = get_data_dir("herde2017", DATA_ROOT_VARIANTS)
-
-	regression.regression_matrix(path.join(data_dir,"SUV_con40-60.csv"),
-		x_cols=x_cols,
-		y_cols=y_cols,
-		x_dict = ROI_ABBREV,
-		y_dict = BEHAVIOUR_ABBREV,
-		df_y_path=path.join(data_dir,"DONOR.csv"),
-		animals=["t1","t2","t3","t4","t5"],
-		output="p_corrected",
-		roi_normalize=False,
-		behav_normalize=False,
-		)
-	regression.regression_matrix(path.join(data_dir,"SUV_con40-60.csv"),
-		x_cols=x_cols,
-		y_cols=y_cols,
-		x_dict = ROI_ABBREV,
-		y_dict = BEHAVIOUR_ABBREV,
-		df_y_path=path.join(data_dir,"DONOR.csv"),
-		animals=["t1","t2","t3","t4","t5"],
-		output="p",
-		roi_normalize=False,
-		behav_normalize=False,
-		)
-	regression.regression_matrix(path.join(data_dir,"SUV_con40-60.csv"),
-		x_cols=x_cols,
-		y_cols=y_cols,
-		x_dict = ROI_ABBREV,
-		y_dict = BEHAVIOUR_ABBREV,
-		df_y_path=path.join(data_dir,"DONOR.csv"),
-		animals=["t1","t2","t3","t4","t5"],
-		output="pearsonr",
-		roi_normalize=False,
-		behav_normalize=False,
-		)
-
 def forced_swim_ttest():
 	import plotting
 
