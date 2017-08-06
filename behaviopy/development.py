06d938a87a753f23b6508cda7d64dd167470c82f
Horea Christian <horea.christ@yandex.com>
2017-08-07 01:03:06 +0200

    migrated sucrosepreference_treatment to stable examples

--- behaviopy/development.py
+++ behaviopy/development.py
@@ -12,20 +12,6 @@ DATA_ROOT_VARIANTS = [
 	path.join(THISSCRIPTSPATH,"..","example_data"),
 	]
 
-def sucrose_preference_treatment():
-	import plotting
-
-	data_dir = get_data_dir("generic", DATA_ROOT_VARIANTS)
-	df_path = path.join(data_dir,"sucrosepreference_treatment.csv")
-	df = pd.read_csv(df_path)
-
-	plotting.expandable_ttest(df,
-		compare="Treatment",
-		comparisons={"Period [days]":[]},
-		datacolumn_label="Sucrose Preference Ratio",
-		rename_treatments={"cFluDW":"Fluoxetine","cFluDW_":"Control"},
-		)
-
 def sucrose_preference_side():
 	import plotting
 
---
+++ behaviopy/examples/sucrosepreference_treatment.py
@@ -0,0 +1,15 @@
+from os import path
+
+from behaviopy.plotting import expandable_ttest
+
+data_dir = path.join(path.dirname(path.realpath(__file__)),"../../example_data/generic")
+sucrose_preference_df_path = path.join(data_dir,"sucrosepreference_treatment.csv")
+
+expandable_ttest(sucrose_preference_df_path,
+	compare="Treatment",
+	comparisons={"Period [days]":[]},
+	datacolumn_label="Sucrose Preference Ratio",
+	rename_treatments={"cFluDW":"Fluoxetine","cFluDW_":"Control"},
+	save_as="sucrosepreference_treatment.pdf",
+	)
+
