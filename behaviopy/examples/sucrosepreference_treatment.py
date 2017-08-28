9e0476aea762d21e80c4ddcf396fdaee195b84a1
Horea Christian <horea.christ@yandex.com>
2017-08-28 15:37:04 +0200

    airier formatting

--- behaviopy/examples/sucrosepreference_treatment.py
+++ behaviopy/examples/sucrosepreference_treatment.py
@@ -6,10 +6,12 @@ data_dir = path.join(path.dirname(path.realpath(__file__)),"../../example_data/g
 sucrose_preference_df_path = path.join(data_dir,"sucrosepreference.csv")
 
 expandable_ttest(sucrose_preference_df_path,
-	compare="Treatment",
-	comparisons={"Period [days]":[]},
-	datacolumn_label="Sucrose Preference Ratio",
-	rename_treatments={"cFluDW":"Fluoxetine","cFluDW_":"Control"},
-	save_as="sucrosepreference_treatment.pdf",
-	)
-
+        compare="Treatment",
+        comparisons={"Period [days]":[]},
+        datacolumn_label="Sucrose Preference Ratio",
+        rename_treatments={
+                "cFluDW":"Fluoxetine",
+                "cFluDW_":"Control"
+                },
+        save_as="sucrosepreference_treatment.pdf",
+        )
