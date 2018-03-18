9a6ff59e53ac7db023fd2ecea92f96e7579b6ea5
Horea Christian <horea.christ@yandex.com>
2018-03-18 06:22:59 +0100

    Added function for data completeness pruning

--- behaviopy/utils.py
+++ behaviopy/utils.py
@@ -9,10 +9,10 @@ def get_data_dir(subdirectory, data_root_variants):
 	----------
 
 	subdirectory: str
-	Which subdirectory (or subsubdirectory, etc.) of the data root is needed
+		Which subdirectory (or subsubdirectory, etc.) of the data root is needed
 
 	data_root_variants: list of str
-	What data root directories to query (in the given order).
+		What data root directories to query (in the given order).
 	"""
 
 	my_data_dir = None
@@ -25,6 +25,42 @@ def get_data_dir(subdirectory, data_root_variants):
 		raise ValueError("No suitable data directory was found on disk.")
 
 	return my_data_dir
+
+def full_subsets(df, minimum_x,
+	unit='subject',
+	x='Session',
+	):
+	"""Return a pruned version of the dataframe containing only `unit` slices complete on `x`.
+
+	Parameters
+	----------
+
+	df : pandas.DataFrame
+		A long-form Pandas Dataframe object which should be pruned.
+	minimum_x : str or list
+		Either the number of entries on `x` qualifying a unit slice as complete, or a list of values, of which the available values on `x` for each value on `unit` should be a superset, in order for the `unit` slice to qualify as complete.
+	unit : str, optional
+		Column of `df` along which `df` is to be sliced and tested for completeness.
+	x : srt, optional
+		Column of `df` on which to check for the presece of sufficient values per `unit` slice.
+
+	Returns
+	-------
+	df
+		A pruned long-form Pandas Dataframe.
+	"""
+	if minimum_x:
+		ids = df[unit].unique()
+		for i in ids:
+			sliced_df = df[df[unit]==i]
+			sessions = sliced_df[x].unique()
+			if isinstance(minimum_x, int):
+				if len(sessions) < minimum_x:
+					df = df[df[unit] != i]
+			else:
+				if not set(minimum_x).issubset(sessions):
+					df = df[df[unit] != i]
+	return df
 
 def perdelta(start, end, delta):
 	curr = start
